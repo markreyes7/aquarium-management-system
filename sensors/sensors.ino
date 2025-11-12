4a387599a7ca7a8a6afce461a1d63c189ba6183a
markreyes7 <mark.reyes7777@gmail.com>
2025-11-11 17:52:48 -0800

    adding temperature updates from sensor

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,78 +1,55 @@
+#include "secrets.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <WiFiS3.h>
 
-#define ONE_WIRE_BUS 8  // DS18B20 data pin connected to digital pin 8
+#define ONE_WIRE_BUS 8  // DS18B20 data pin connected to pin D8
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-char WIFI_SSID[] = //wifi_name
-char WIFI_PASS[] = //pass
-
-char serverAddress[] = "localhost"; 
-int serverPort = 3001;
+char serverAddress[] = "192.168.1.55"; // my thinkpad's IP address
+int serverPort = 3001;                 // Flask port
 
 void setup() {
   Serial.begin(9600);
   sensors.begin();
 
+  Serial.print("Connecting to WiFi...");
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi...");
+    Serial.print(".");
   }
-  Serial.println("WiFi connected!");
-  Serial.print("IP Address: ");
+  Serial.println("\nWiFi connected!");
+  Serial.print("Device IP: ");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
   sensors.requestTemperatures();
   float tempF = sensors.getTempFByIndex(0);
+  Serial.print("Temp: ");
+  Serial.print(tempF);
+  Serial.println(" °F");
 
-  Serial.print("Temperature (°F): ");
-  Serial.println(tempF);
-
-  // Build JSON payload
   String postData = "{\"temperature\": " + String(tempF, 2) + "}";
 
-  // TODO fix this.
   WiFiClient client;
   if (client.connect(serverAddress, serverPort)) {
-    Serial.println("Connected to Flask server");
-
     client.println("POST /update/temp HTTP/1.1");
     client.print("Host: ");
     client.println(serverAddress);
     client.println("Content-Type: application/json");
     client.print("Content-Length: ");
     client.println(postData.length());
-    client.println();  
-    client.print(postData); 
-
-    
-    unsigned long timeout = millis();
-    while (client.connected() && !client.available()) {
-      if (millis() - timeout > 5000) {
-        Serial.println(">>> Timeout waiting for server");
-        client.stop();
-        return;
-      }
-      delay(10);
-    }
-
-   
-    while (client.available()) {
-      char c = client.read();
-      Serial.write(c);
-    }
-
+    client.println();
+    client.print(postData);
     client.stop();
-    Serial.println("\nDisconnected from server");
+    Serial.println("Sent to Flask!");
   } else {
-    Serial.println("Failed to connect to Flask server");
+    Serial.println("Failed to connect to Flask");
   }
 
-  delay(5000);
+  delay(30000); 
 }
